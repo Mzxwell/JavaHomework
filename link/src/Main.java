8cbc5723c56b1246133eea55d6995034d6a2b047
Maxwell <1979829742@qq.com>
2023-10-24 11:53:33 +0800

    homework3

--- link/src/Main.java
+++ link/src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        linkList linkList = new linkList(new node());
-        for (int i=1;i<4;i++){linkList.add(new node(i,null,null));}
-        linkList.print();
-        linkList.swap(linkList.head).print();
-        linkList.swap(linkList.head.righ.righ).print();
+        linkList list = new linkList(new node());
+        for (int i=1;i<4;i++){list.add(new node(i,null,null));}
+        list.print();
+        list.swap(list.head).print();
+        list.swap(list.head.righ.righ).print();
         DLL dll =  new DLL(new node());
         for (int i=1;i<4;i++){dll.add(new node(i,null,null));}
         dll.print();
@@ -14,5 +14,20 @@ public class Main {
         dll.print(dll.tail);
         dll.swap(dll.head.righ.righ).print();
         dll.print(dll.tail);
+        linkList linkList2 = new linkList(new node());
+        for (int i=2;i<30;i+=2)linkList2.add(new node(i,null,null));
+        linkList linkList3 = new linkList(new node());
+        for (int i=3;i<30;i+=3)linkList3.add(new node(i,null,null));
+        linkList2.print();
+        linkList3.print();
+        linkList.intersect(linkList2,linkList3).print();
+        linkList2.print();
+        linkList3.print();
+        linkList u=linkList.union(linkList2,linkList3);
+        u.print();
+        linkList2.print();
+        linkList3.print();
+        u.reverse().print();
+        linkList3.reverse().print();
     }
 }
--- link/src/linkList.java
+++ link/src/linkList.java
@@ -1,6 +1,10 @@
+import org.jetbrains.annotations.NotNull;
+
 public class linkList {
     public node head, tail;
-    public linkList(){}
+
+    public linkList() {
+    }
 
     public linkList(node head) {
         this.head = head;
@@ -11,6 +15,11 @@ public class linkList {
     }
 
     public void add(node node) {
+        if (head == null) {
+            head = node;
+            tail = node;
+            return;
+        }
         tail.righ = node;
         tail = tail.righ;
     }
@@ -18,7 +27,7 @@ public class linkList {
     public void print() {
         node cursor = head;
         while (cursor != null) {
-            System.out.print(cursor.num);
+            System.out.print(cursor.num + " ");
             cursor = cursor.righ;
         }
         System.out.println();
@@ -26,16 +35,62 @@ public class linkList {
 
     public linkList swap(node left) {
         node previous = head;
-        if (head.equals(left)){
-            head=left.righ;
-            previous.righ =head.righ;
-            head.righ =previous;
-        }else {
-            while (!previous.righ.equals(left)){previous=previous.righ;}
-            previous.righ =left.righ;
-            left.righ =left.righ.righ;
-            previous.righ.righ =left;
+        if (head.equals(left)) {
+            head = left.righ;
+            previous.righ = head.righ;
+            head.righ = previous;
+        } else {
+            while (!previous.righ.equals(left)) {
+                previous = previous.righ;
+            }
+            previous.righ = left.righ;
+            left.righ = left.righ.righ;
+            previous.righ.righ = left;
         }
+        return this;
+    }
+
+    public static @NotNull linkList intersect(@NotNull linkList L1, @NotNull linkList L2) {
+        node L1C = L1.head, L2C = L2.head;
+        linkList result = new linkList();
+        while (L1C != null && L2C != null) {
+            if (L1C.num == L2C.num) result.add(new node(L1C.num, null, null));
+            if (L1C.num <= L2C.num) L1C = L1C.righ;
+            else L2C = L2C.righ;
+        }
+        return result;
+    }
+
+    public static linkList union(@NotNull linkList L1, @NotNull linkList L2) {
+        node L1C = L1.head, L2C = L2.head, cursor;
+        linkList result;
+        for (result = new linkList(new node()); L1C != null && L2C != null; )
+            if (L1C.num < L2C.num) {
+                result.add(new node(L1C.num, null, null));
+                L1C = L1C.righ;
+            } else {
+                result.add(new node(L2C.num, null, null));
+                L2C = L2C.righ;
+            }
+        for (cursor = L1C == null ? L2C : L1C; cursor != null; cursor = cursor.righ)
+            result.add(new node(cursor.num, null, null));
+        if (result.head != null) for (cursor = result.head; cursor.righ != null; cursor = cursor.righ)
+            while (cursor.num == cursor.righ.num) cursor.righ = cursor.righ.righ;
+        return result;
+    }
+    public linkList reverse(){
+        if(head==null||head.righ==null)return this;
+        node c1=head,c2 = c1.righ,c3=c2.righ;
+        c1.righ=null;
+        tail=c1;
+        c2.righ=c1;
+        while (c3!=null){
+            c1=c2;
+            c2=c3;
+            c3=c3.righ;
+            c2.righ=c1;
+        }
+        head=c2;
         return this;
     }
 }
